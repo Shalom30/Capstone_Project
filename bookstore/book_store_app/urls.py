2451d9f63e60207d9976428f77961842443ecce3
Wepngong Shalom <wepngongshalom23@gmail.com>
2025-03-29 09:23:58 +0100

    project

--- bookstore/book_store_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from book_store_app import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-]
--- firstproject/passgen/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PassgenConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'passgen'

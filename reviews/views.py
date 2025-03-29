7015d8c19494eb9d62372a7c5a0db8fb7d76981b
Wepngong Shalom <wepngongshalom23@gmail.com>
2025-03-29 09:12:53 +0100

    creating and installing the django-filter for my project and also working on the views.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Review
+from .serializers import ReviewSerializer, UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+# User CRUD
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+# Review CRUD
+class ReviewListCreateView(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['movie_title', 'rating']
+    search_fields = ['movie_title', 'review_content']
+    ordering_fields = ['rating', 'created_date']
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_update(self, serializer):
+        if serializer.instance.user != self.request.user:
+            raise permissions.PermissionDenied("You can only edit your own reviews.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance.user != self.request.user:
+            raise permissions.PermissionDenied("You can only delete your own reviews.")
+        instance.delete()
